9753195bf67c0cb56f89451f5e1975b35a273569
mediaotg <esty.mediaotg@gmail.com>
2017-10-18 17:11:03 -0400

    Login, logout, menu...

--- js/global.js
+++ js/global.js
@@ -1,13 +1,12 @@
 /* Global js */
 $(document).ready(function(e){
 
-  // Check if logged in
   function getCookie(name){
     var cookie = document.cookie;
     var prefix = name + "=";
     var begin = cookie.indexOf("; " + prefix);
     if(begin == -1) {
-      begin = dc.indexOf(prefix);
+      begin = cookie.indexOf(prefix);
       if(begin != 0) return null;
     } else {
       begin += 2;
@@ -18,8 +17,71 @@ $(document).ready(function(e){
     }
     return decodeURI(cookie.substring(begin + prefix.length, end));
   }
+  function delCookie(name) {
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+  };
 
+  // Hide loader
+  setTimeout(function(){
+    $('.loader-screen').fadeOut("fast", function(){});
+  }, 100);
+
+  // Check if logged in
   function checkLogin(){
-    // var cookie = getCookie("")
+    var cookie = getCookie("user");
+    if(cookie != null && cookie.length > 0){
+      var user = JSON.parse(cookie);
+      var initial = user.username.charAt(0);
+      $('.user .icon').text(initial.toUpperCase());
+    } else {
+      window.location.href = "index.html";
+    }
   }
+  checkLogin();
+
+  // Minimize Nav
+  $('.nav-opener').click(function(e){
+    $('nav').css('width', '80px');
+    $('nav').toggleClass('w-25');
+    $('nav .nav-link .text').toggle();
+
+    $('.content').css('width', 'calc(100% - 80px)');
+    $('.content').toggleClass('w-75');
+
+    var open = !(getCookie("nav") == 'true');
+    if(open === null) {open = false;}
+    document.cookie = "nav=" + open;
+  });
+
+  if(getCookie("nav") == 'true'){
+    $('nav').css('width', '80px');
+    $('nav').toggleClass('w-25');
+    $('nav .nav-link .text').toggle();    
+
+    $('.content').css('width', 'calc(100% - 80px)');
+    $('.content').toggleClass('w-75');
+  }
+
+  // Logout Popup
+  $('.user').click(function(e){
+    $('.logout-popup').toggle();
+      e.stopPropagation();
+  });
+  $(window).click(function() {
+    $('.logout-popup').hide();
+  });
+  $('.logout-popup').click(function(e){
+    e.stopPropagation();
+  });
+
+  // Logout
+  $('#logout').click(function(e){
+    e.preventDefault();
+
+    delCookie("user");
+    delCookie("nav");
+
+    window.location.href="index.html";
+  });
+
 });
