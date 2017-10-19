fa35ed173812097166437867a7df772a4e9c31d7
mediaotg <esty.mediaotg@gmail.com>
2017-10-19 10:40:36 -0400

    heroku

--- php/index.php
+++ php/index.php
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Import Demo</title>
-  </head>
-  <body>
-    <form action="import.php" method="post" enctype="multipart/form-data">
-      <input type="file" name="upfile" id="upfile">
-      <input type="submit" value="Upload Image" name="submit">
-    </form>
-  </body>
-</html>
+<?php header( 'Location: /form.html' ) ;  ?>
