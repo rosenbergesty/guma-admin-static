2a299f942e42edcc072353d274f176792a747be4
mediaotg <esty.mediaotg@gmail.com>
2017-11-01 16:56:41 -0400

    wip: fetch-drivers.php return 30 at a time

---
+++ php/fetch-drivers.php
@@ -0,0 +1,36 @@
+<?php
+  // Fetch drivers
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Headers: Content-Type");
+
+  // Connect to db
+  require_once('connect.php');
+
+  // Get data
+  $response = [];
+
+  $count = '';
+  if( isset($_POST['count']) ){
+    $count = $_POST['count']; 
+  } else {
+    $requestBody = file_get_contents('php://input');
+    $parsed = json_decode($requestBody, true);
+    $count = $parsed['count'];
+  }
+
+  // Insert dispatcher
+  $sql = "SELECT * FROM drivers";
+  $result = $conn->query($sql);
+  if($result->num_rows > 0){
+    while($row = $result->fetch_assoc()){
+      $array = array("ID"=>$row["ID"], "name"=>$row["name"], "email"=>$row["email"], "password"=>$row["password"] );
+      array_push($response, $array);
+    }
+  } else {
+    $response = '0 results';
+  }
+
+  echo json_encode($response);
+
+  $conn->close();
+?>
